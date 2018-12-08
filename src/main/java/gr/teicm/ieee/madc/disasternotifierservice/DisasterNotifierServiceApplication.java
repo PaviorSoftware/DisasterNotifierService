d501c0ed31f20408803d86b5751c869c05f7ec2a
Iordanis Kostelidis <iordaniskostelidis@protonmail.com>
2018-12-08 23:00:20 +0200

    Initial commit

---
+++ src/main/java/gr/teicm/ieee/madc/disasternotifierservice/DisasterNotifierServiceApplication.java
@@ -0,0 +1,12 @@
+package gr.teicm.ieee.madc.disasternotifierservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisasterNotifierServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DisasterNotifierServiceApplication.class, args);
+    }
+}
