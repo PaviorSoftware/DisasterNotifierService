4f8da52f2dcaa2b079f2a556535ec4553aefc460
Iordanis Kostelidis <iordaniskostelidis@protonmail.com>
2018-12-15 02:04:26 +0200

    Revert "Third Release"
    This reverts commit 5bca4f0f

--- src/main/java/gr/teicm/ieee/madc/disasternotifierservice/service/rest/impl/DisasterServiceImpl.java
+++ src/main/java/gr/teicm/ieee/madc/disasternotifierservice/service/rest/impl/DisasterServiceImpl.java
@@ -1,17 +1,21 @@
 package gr.teicm.ieee.madc.disasternotifierservice.service.rest.impl;
 
 import gr.teicm.ieee.madc.disasternotifierservice.commons.exception.EntityNotFoundException;
-import gr.teicm.ieee.madc.disasternotifierservice.commons.exception.NotImplementedException;
+import gr.teicm.ieee.madc.disasternotifierservice.commons.exception.ForbiddenException;
 import gr.teicm.ieee.madc.disasternotifierservice.commons.exception.UnauthorizedException;
-import gr.teicm.ieee.madc.disasternotifierservice.domain.embeddable.Location;
 import gr.teicm.ieee.madc.disasternotifierservice.domain.entity.Disaster;
 import gr.teicm.ieee.madc.disasternotifierservice.domain.entity.User;
 import gr.teicm.ieee.madc.disasternotifierservice.domain.repository.DisasterRepository;
+import gr.teicm.ieee.madc.disasternotifierservice.model.DisasterRange;
+import gr.teicm.ieee.madc.disasternotifierservice.service.commons.DisasterRadiusService;
+import gr.teicm.ieee.madc.disasternotifierservice.service.commons.DistanceService;
 import gr.teicm.ieee.madc.disasternotifierservice.service.rest.DisasterService;
+import gr.teicm.ieee.madc.disasternotifierservice.service.rest.UserService;
 import gr.teicm.ieee.madc.disasternotifierservice.service.security.AuthService;
 import org.springframework.stereotype.Service;
 
 import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -20,15 +24,31 @@ public class DisasterServiceImpl implements DisasterService {
 
     private final DisasterRepository disasterRepository;
     private final AuthService authService;
+    private final UserService userService;
+    private final DistanceService distanceService;
+    private final DisasterRadiusService disasterRadiusService;
 
-    public DisasterServiceImpl(DisasterRepository disasterRepository, AuthService authService) {
+    public DisasterServiceImpl(DisasterRepository disasterRepository, AuthService authService, UserService userService, DistanceService distanceService, DisasterRadiusService disasterRadiusService) {
         this.disasterRepository = disasterRepository;
         this.authService = authService;
+        this.userService = userService;
+        this.distanceService = distanceService;
+        this.disasterRadiusService = disasterRadiusService;
     }
 
     @Override
-    public List<Disaster> get(String accessToken) {
-        return disasterRepository.findAll();
+    public List<Disaster> get(String authorization) {
+        List<Disaster> all = disasterRepository.findAll();
+        List<Disaster> activeAll = new ArrayList<>();
+
+        for (Disaster disaster : all) {
+            if (disaster.isActive()) {
+                activeAll.add(disaster);
+            }
+        }
+
+        return activeAll;
+
     }
 
     @Override
@@ -37,33 +57,71 @@ public class DisasterServiceImpl implements DisasterService {
     }
 
     @Override
-    public Disaster get(Long id, String accessToken) throws EntityNotFoundException {
+    public List<Disaster> near(User user, Long wantedDistance) {
+
+        List<Disaster> all = disasterRepository.findAll();
+        List<Disaster> near = new ArrayList<>();
+
+        for (Disaster disaster : all) {
+            if (distanceService.get(disaster.getDisasterLocation(), user.getLocation()) < wantedDistance) {
+                near.add(disaster);
+            }
+        }
+
+        return near;
+    }
+
+    @Override
+    public Disaster get(Long id, String authorization) throws EntityNotFoundException {
         return getDisasterByIdOrElseThrow(id, new EntityNotFoundException());
     }
 
     @Override
-    public Disaster post(Disaster disaster, String accessToken) throws UnauthorizedException, NoSuchAlgorithmException {
+    public Disaster post(Disaster disaster, String authorization) throws UnauthorizedException, NoSuchAlgorithmException {
+        User user = authService.getUser(authorization);
 
-        if (disaster.getSafeLocation() == null) {
-            Location safeLocation = new Location();
-            safeLocation.setLatitude((float) 0);
-            safeLocation.setLongitude((float) 0);
-        }
+        calculateAndSetRadius(disaster);
 
-        User user = authService.getUser(accessToken);
         disaster.setCreator(user);
+        disaster.setActive(true);
 
         return disasterRepository.save(disaster);
     }
 
     @Override
-    public Disaster put(Long id, Disaster disaster, String accessToken) throws NotImplementedException {
-        throw new NotImplementedException();
+    public Disaster put(Long id, Disaster disaster, String authorization) throws UnauthorizedException, NoSuchAlgorithmException, EntityNotFoundException, ForbiddenException {
+        checkIfDisasterExistsAndAuthorizedAndHasRights(id, authorization);
+        disaster.setId((id));
+
+        calculateAndSetRadius(disaster);
+
+        return disasterRepository.save(disaster);
+    }
+
+    private void calculateAndSetRadius(Disaster disaster) {
+        DisasterRange calculatedRadiusSet = disasterRadiusService.calculate(disaster.getRedRadius());
+
+        disaster.setRedRadius(calculatedRadiusSet.getRed());
+        disaster.setYellowRadius(calculatedRadiusSet.getOrange());
+        disaster.setGreenRadius(calculatedRadiusSet.getGreen());
     }
 
     @Override
-    public void delete(Long id, String accessToken) throws NotImplementedException {
-        throw new NotImplementedException();
+    public void delete(Long id, String authorization) throws EntityNotFoundException, UnauthorizedException, NoSuchAlgorithmException, ForbiddenException {
+        checkIfDisasterExistsAndAuthorizedAndHasRights(id, authorization);
+        disasterRepository.deleteById(id);
+    }
+
+    private void checkIfDisasterExistsAndAuthorizedAndHasRights(Long id, String accessToken) throws EntityNotFoundException, UnauthorizedException, NoSuchAlgorithmException, ForbiddenException {
+        Disaster dbDisaster = getDisasterByIdOrElseThrow(id, new EntityNotFoundException());
+        User user = authService.getUser(accessToken);
+        checkIfIsCreatorOrAdminElseThrow(dbDisaster, user, new ForbiddenException());
+    }
+
+    private void checkIfIsCreatorOrAdminElseThrow(Disaster dbDisaster, User user, ForbiddenException e) throws ForbiddenException {
+        if (!dbDisaster.getCreator().equals(user) && userService.isAdmin(user)) {
+            throw e;
+        }
     }
 
     private Disaster getDisasterByIdOrElseThrow(Long id, EntityNotFoundException e) throws EntityNotFoundException {
@@ -74,6 +132,7 @@ public class DisasterServiceImpl implements DisasterService {
         }
 
         return byId.get();
-
     }
+
+
 }
